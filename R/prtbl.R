2c808f5b30b47a24fcb4c54ef71b0f63e034cf75
MarcellGranat <granatcellmar98@gmail.com>
2020-11-17 15:43:23 +0100

    10-17-4

---
+++ R/prtbl.R
@@ -0,0 +1,22 @@
+#' @title overview_tab
+#'
+#' @description Provides an overview table for the time and scope conditions of
+#'     a data set
+#'
+#' @param dat A data set object
+#' @param id Scope (e.g., country codes or individual IDs)
+#' @param time Time (e.g., time periods are given by years, months, ...)
+#'
+#' @return A data frame object that contains a summary of a sample that
+#'     can later be converted to a TeX output using \code{overview_print}
+#' @examples
+#' data(toydata)
+#' output_table <- overview_tab(dat = toydata, id = ccode, time = year)
+#' @export
+#' @importFrom dplyr "%>%"
+#'
+
+
+prtbl <- function(x) {
+  x^3
+}
