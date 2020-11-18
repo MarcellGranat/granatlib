f1f3b80f9738680a94d188e713f53a29e36d7d4a
MarcellGranat <granatcellmar98@gmail.com>
2020-11-18 12:24:00 +0100

    gg_ppt

---
+++ R/gg_ppt.R
@@ -0,0 +1,20 @@
+#' @title gg_ppt
+#'
+#' @description Copies a gg object into a new powerpoint slide.
+#' @param x A gg object (.last.value)
+#' @param filename Name of the slide. '.pptx' is redundant here. (plot)
+#'
+#'\code{overview_print}
+#' @examples
+#' ggplot(cars, aes(speed, dist)) + geom_point()
+#' gg_ppt()
+#' @export
+#'
+
+gg_ppt <- function(x = .Last.value, filename = "plot") {
+  x <- rvg::dml(ggobj = x)
+  doc <- officer::read_pptx()
+  doc <- officer::add_slide(doc, layout = "Title and Content", master = "Office Theme")
+  doc <- officer::ph_with(doc, x, location = officer::ph_location_fullsize())
+  print(doc, target = paste0(filename,".pptx"))
+}
