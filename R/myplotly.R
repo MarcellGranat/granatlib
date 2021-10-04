fe94e273996612f332cde21878ea01f484906259
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2021-10-04 13:01:31 +0200

    initial update

--- R/myplotly.R
+++ R/myplotly.R
@@ -1,4 +1,20 @@
-myplotly <- function(x = NULL, ) {
-  plotly::ggplotly(p = x) %>%
-    plotly::config(displayModeBar = F)
+myplotly <- function(p, tooltip = c("text"), bgcolor = "#2DA2BF") {
+
+    font = list(
+      family = "Alright Sans Regular",
+      size = 15,
+      color = "black"
+    )
+
+    label = list(
+      bgcolor = bgcolor,
+      bordercolor = "transparent",
+      font = font
+    )
+
+    plotly::ggplotly(p, tooltip = tooltip) %>%
+      plotly::style(hoverlabel = label) %>%
+      plotly::layout(font = font) %>%
+      plotly::config(displayModeBar = FALSE)
+
 }
