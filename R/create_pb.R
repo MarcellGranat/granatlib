0998a4881b9265ebde3bdc38f458b4ff3178d6c0
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2022-07-19 02:03:09 +0200

    debug

--- R/create_pb.R
+++ R/create_pb.R
@@ -10,25 +10,34 @@ create_pb <- function(x) {
 
   if (is.data.frame(x)) {
     n = nrow(x)
+    retrive <- TRUE
   } else if (is.numeric(x) & length(x) == 1) {
     n = x
+    retrive <- FALSE
   } else {
     n = length(x)
+    retrive <- TRUE
   }
 
   library(progress)
 
-    pb <- progress_bar[["new"]](format = "(:spin) [:bar] :percent [Elapsed time: :elapsedfull || Estimated time remaining: :eta]",
-                                total = n,
-                                complete = "=",
-                                incomplete = "-",
-                                current = ">",
-                                force = TRUE,
-                                clear = FALSE,
-                                width = 100)
+  pb <- progress_bar[["new"]](format = "(:spin) [:bar] :percent [Elapsed time: :elapsedfull || Estimated time remaining: :eta]",
+                              total = n,
+                              complete = "=",
+                              incomplete = "-",
+                              current = ">",
+                              force = TRUE,
+                              clear = FALSE,
+                              width = 100)
 
-    assign("pb", pb, envir = rlang::global_env())
-    if (!exists("pb")) assign("pb", pb, envir = rlang::env_parent())
+  assign("pb", pb, envir = rlang::global_env())
+  if (!exists("pb")) assign("pb", pb, envir = rlang::env_parent())
+
+  if (exists("pb")) {
     message(crayon::green("pb created"))
+  }
 
+  if (retrive) {
+    return(x)
+  }
 }
