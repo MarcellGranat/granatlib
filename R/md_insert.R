b94eda4b5f95edcf9d56c0a0d2149e6581c0a7f6
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2022-07-25 10:37:41 +0200

    text_contain in md_insert

--- R/md_insert.R
+++ R/md_insert.R
@@ -7,10 +7,24 @@
 #' @export
 #'
 
-md_insert <- function(md_name) {
+md_insert <- function(md_name, text_contained = NULL) {
+
+  if (is.null(text_contained)) {
+    if (exists("params$text_contained")) {
+      text_contained = params$text_contained
+    } else {
+      text_contained = TRUE
+    }
+  }
+
+  if (text_contained) {
+
   read_delim(md_name, delim = "+_+_+_+%76324189", col_names = FALSE)[[1]] %>% # read all the lines
     {ifelse(str_starts(., "#"), str_c("\n", .), .)} %>%
     {str_c(., "\n\n", collapse = "")}
+  }
 
   # TODO citation replace
+    # citet!
+    # full change if latex
 }
