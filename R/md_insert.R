40ed5d8734e599cf7d034130a83cd90210cf394d
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2022-07-28 11:25:54 +0200

    debug

--- R/md_insert.R
+++ R/md_insert.R
@@ -9,7 +9,7 @@
 
 md_insert <- function(x, text_contained = NULL) {
 
-  if (stringr::str_ends(x, ".md")) {
+  if (str_ends(x, ".md")) {
 
   if (is.null(text_contained)) { # if not given explicitly
 
