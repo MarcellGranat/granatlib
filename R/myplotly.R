0faafb139a4ac35beb0a1dec1595f3f9184973a4
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2022-08-01 14:41:30 +0200

    render based outcome

--- R/myplotly.R
+++ R/myplotly.R
@@ -1,20 +1,24 @@
 myplotly <- function(p, tooltip = c("text"), bgcolor = "#2DA2BF") {
 
-    font = list(
-      family = "Alright Sans Regular",
-      size = 15,
-      color = "black"
-    )
+  font = list(
+    family = "Alright Sans Regular",
+    size = 15,
+    color = "black"
+  )
 
-    label = list(
-      bgcolor = bgcolor,
-      bordercolor = "transparent",
-      font = font
-    )
+  label = list(
+    bgcolor = bgcolor,
+    bordercolor = "transparent",
+    font = font
+  )
 
+  if (knitr::is_latex_output() | knitr::pandoc_to("gfm-yaml_metadata_block", "docx")) {
+    p
+  } else {
     plotly::ggplotly(p, tooltip = tooltip) %>%
       plotly::style(hoverlabel = label) %>%
       plotly::layout(font = font) %>%
       plotly::config(displayModeBar = FALSE)
+  }
 
 }
