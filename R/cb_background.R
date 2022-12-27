d3edf190ecbd3f7a646c051f2ed7d9c127958549
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2022-12-27 16:30:19 +0100

    background function

---
+++ R/cb_background.R
@@ -0,0 +1,53 @@
+cb_background <- function(name = NULL, remove_temp_code = FALSE, sleep_time = 7) {
+  
+  if (is.null(name)) {
+    name <- clipr::read_clip() %>% 
+      keep(str_detect, "\\w") %>% 
+      str_remove("<-.*") %>% 
+      first()
+  }
+  
+  temp_file_name <- str_remove_all(Sys.time(), "\\D") %>% 
+    str_c("_temp_code.R")
+  
+  temp_code <- str_c('
+message(crayon::bgMagenta("Running code from clipboard"))
+message(crayon::cyan(str_flatten(clipr::read_clip(allow_non_interactive = TRUE), "\n")))
+tictoc::tic()
+tryCatch({
+',
+  str_flatten(clipr::read_clip(allow_non_interactive = TRUE), "\n"),
+'
+}, error = function(e) {
+ e <<- e
+})
+
+if (exists("e")) {
+  suppressWarnings({
+    granatlib::notification(msg = as.character(e), sound = TRUE)
+  })
+  stop(as.character(e), call. = FALSE)
+}
+
+toc_msg <- capture.output(tictoc::toc())
+granatlib::notification(msg = toc_msg, sound = TRUE)
+message(crayon::bgGreen("Succeeded."))
+message(crayon::bgCyan(toc_msg))
+rm("toc_msg")
+')
+  
+  cat(temp_code, file = temp_file_name)
+  
+  rstudioapi::jobRunScript(
+    temp_file_name, 
+    name = name, 
+    importEnv = TRUE,
+    exportEnv = "R_GlobalEnv"
+    )
+  
+  if (remove_temp_code) {
+    Sys.sleep(sleep_time)
+    file.remove(temp_file_name)
+  }
+  
+}
---
+++ R/script_background.R
@@ -0,0 +1,50 @@
+script_background <- function(script_name = NULL, name = NULL, ..., remove_temp_code = TRUE, sleep_time = 7) {
+  
+if (is.null(name)) {
+  name <- script_name %>% 
+    str_remove("[.]R") %>% 
+    str_replace_all("-", " ") %>% 
+    str_remove("\\d* ") %>% 
+    str_to_title()
+}
+
+if (is.null(script_name)) {
+  stop("Provide an R script name.")
+}
+
+temp_file_name <- str_remove_all(Sys.time(), "\\D") %>% 
+  str_c("_temp_code.R")
+
+temp_code <- str_c('
+message(crayon::bgMagenta("Running ", "',script_name,'" ))
+tictoc::tic(msg = str_remove("',script_name,'", "[.]R"))
+tryCatch({
+  source("',script_name,'")
+}, error = function(e) {
+ e <<- e
+})
+
+if (exists("e")) {
+  suppressWarnings({
+    notification(msg = as.character(e), sound = TRUE)
+  })
+  stop(as.character(e), call. = FALSE)
+}
+
+toc_msg <- capture.output(tictoc::toc())
+notification(msg = toc_msg, sound = TRUE)
+message(crayon::bgGreen("Succeeded."))
+message(crayon::bgCyan(toc_msg))
+rm("toc_msg")
+')
+
+cat(temp_code, file = temp_file_name)
+
+rstudioapi::jobRunScript(temp_file_name, name = name, ...)
+
+if (remove_temp_code) {
+  Sys.sleep(sleep_time)
+  file.remove(temp_file_name)
+}
+
+}
