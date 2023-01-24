380c97205ad4a0815275c6c26ba719cdde4438e2
MarcellGranat <granatcellmar98@gmail.com>
2023-01-24 20:32:10 +0100

    conditional message

--- R/n_times_try.R
+++ R/n_times_try.R
@@ -1,11 +1,13 @@
-n_times_try <- function(.exp, sleep_times = c(3, 3, 3), otherwise = NULL) {
+n_times_try <- function(.exp, sleep_times = c(3, 3, 3), otherwise = NULL, print_message = FALSE) {
   for (st in sleep_times) {
-    tryCatch({out <- {.exp}}, error = \(e) message(crayon::magenta(e), "\n"))
+    tryCatch({out <- {.exp}}, error = \(e) if (print_message) message("\n", crayon::magenta(e)))
     if (exists("out")) {
       return(out)
       break
     }
-    message("Run into error, wait ", crayon::blue(st), " sec")
+    if (print_message) {
+      message("Wait ", crayon::blue(st), " sec")
+    }
     Sys.sleep(st)
   }
   message(crayon::red("FATAL"))
