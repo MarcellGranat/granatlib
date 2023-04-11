5b0faf748e7a4ae6de1610547132b63b12615d96
MarcellGranat <granatcellmar98@gmail.com>
2023-04-11 11:57:29 +0200

    theme update

--- R/theme_gR.R
+++ R/theme_gR.R
@@ -2,8 +2,6 @@
 #'
 #' @description A theme that is conditionally dark or minimal (by default). Dark if you use it in RStudio.
 #'
-#' @param base_theme A theme to use
-#'
 #' @examples
 #' ggplot(iris, aes(Sepal.Length, Petal.Width)) +
 #'   geom_point() +
@@ -11,30 +9,24 @@
 #'
 #' @export
 
-theme_gR <- function(base_theme = NULL) {
+theme_gR <- function() {
 
-  if (is.null(base_theme)) {
-    if (.Platform$GUI == "RStudio") {
-      base_theme <- ggdark::dark_theme_minimal() +
-        theme(
-          plot.background = element_rect(fill = "#161616", color = "#161616"),
-          panel.border = element_blank(),
-          panel.grid.major = element_line(color = "grey25"),
-          panel.grid.minor = element_line(color = "grey25")
-        )
-    } else {
-      base_theme <- ggplot2::theme_minimal()
-    }
+  if (knitr::is_latex_output() | knitr::pandoc_to("docx") | knitr::is_html_output()) {
+    ggplot2::theme_get() # only in RStudio
+  } else {
+    ggdark::dark_theme_minimal() +
+      theme(
+        plot.background = element_rect(fill = "#161616", color = "#161616"),
+        panel.border = element_blank(),
+        panel.grid.major = element_line(color = "grey25"),
+        panel.grid.minor = element_line(color = "grey25"),
+        legend.position = "bottom",
+        plot.title.position = "plot",
+        plot.title = element_text(face = "bold", hjust = .5),
+        plot.tag.position = "topright",
+        plot.caption.position = "plot",
+        legend.key = element_blank(),
+        legend.box = "vertical"
+      )
   }
-
-  base_theme +
-    theme(
-      legend.position = "bottom",
-      plot.title.position = "plot",
-      plot.title = element_text(face = "bold", hjust = .5),
-      plot.tag.position = "topright",
-      plot.caption.position = "plot",
-      legend.key = element_blank(),
-      legend.box = "vertical"
-    )
 }
